3e1632dc369406546a690f9adba084d89d60a49d
upendhar Nemmani <upendharnemmani@gmail.com>
2024-05-13 13:11:41 +0530

    v-1.6 - resume-link  updated

--- src/components/Header/Hero.jsx
+++ src/components/Header/Hero.jsx
@@ -16,9 +16,9 @@ const Hero = () => {
             <p className='font-bold text-3xl'>Nemmani Upendhar</p>
             <p className='font-medium text-2xl'>Software Engineer</p>
             <p>Based in Hyderabad, India</p>
-            <a href='https://drive.google.com/file/d/1TJ7cvhb8819SCKNX4SSiSDzLiG3BcKBv/view?usp=sharing' target='_blank' className='p-2 w-[50%] md:w-[15vw] rounded-xl font-bold text-white bg-black hover:scale-110 hover:transition text-center'>View Resume</a>
+            <a href='https://drive.google.com/file/d/1Sd9aZgTGMEaZGUsQzT3as6uW4GT-Bev7/view?usp=sharing' target='_blank' className='p-2 w-[50%] md:w-[15vw] rounded-xl font-bold text-white bg-black hover:scale-110 hover:transition text-center'>View Resume</a>
         </div>
-        <div className='sm:hidden flex absolute right-5 bottom-[22%] flex-col gap-8 justify-center text-4xl'>
+        <div className='sm:hidden flex absolute right-2 bottom-[22%] flex-col gap-8 justify-center text-4xl'>
             <a href='https://www.linkedin.com/in/upendharnemmani' target='_blank' className="hover:scale-110 hover:transition-all">
             <FontAwesomeIcon icon={faLinkedin} /></a>
             <a href='https://github.com/Upendhar10' target='_blank' className="hover:scale-110 hover:transition-all">
--- src/components/Header/Menu.jsx
+++ src/components/Header/Menu.jsx
@@ -5,10 +5,10 @@ import { faUser , faGraduationCap , faHandshake, faSheetPlastic} from '@fortawes
 function Menu() {
   return (
     <div className='flex gap-10 text-5xl items-center bg-[#f5f6fa] border-t-2 py-4 rounded md:hidden sticky bottom-0 left-[6%] justify-center '>
-        <a href="#About" className="p-1 hover:scale-110 hover:transition-all hover:text-sky-400"> <FontAwesomeIcon icon={faUser} /> </a>
-        <a href="#Qualification" className="p-1 hover:scale-110 hover:transition-all hover:text-sky-400"> <FontAwesomeIcon icon={faGraduationCap} /> </a> 
-        <a href="#Projects" className="p-1 hover:scale-110 hover:transition-all hover:text-sky-400"> <FontAwesomeIcon icon={faSheetPlastic} /> </a>
-        <a href="#Connect" className="p-1 hover:scale-110 hover:transition-all hover:text-sky-400"> <FontAwesomeIcon icon={faHandshake} /></a>
+        <a href="#About" className="py-1 hover:scale-110 hover:transition-all hover:text-sky-400"> <FontAwesomeIcon icon={faUser} /> </a>
+        <a href="#Qualification" className="py-1 hover:scale-110 hover:transition-all hover:text-sky-400"> <FontAwesomeIcon icon={faGraduationCap} /> </a> 
+        <a href="#Projects" className=" py-1 hover:scale-110 hover:transition-all hover:text-sky-400"> <FontAwesomeIcon icon={faSheetPlastic} /> </a>
+        <a href="#Connect" className="py-1 hover:scale-110 hover:transition-all hover:text-sky-400"> <FontAwesomeIcon icon={faHandshake}/></a>
     </div>
   )
 }
